c06092271a39288aa2a05d2804509efce037b435
Gustavo Matheus <guuhsykees@gmail.com>
2023-10-20 03:31:52 -0300

    build: medic and specialty controller and model

---
+++ app/Http/Controllers/MedicController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medic;
+use Illuminate\Http\Request;
+
+class MedicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Medic $medic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Medic $medic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Medic $medic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Medic $medic)
+    {
+        //
+    }
+}
---
+++ app/Models/Medic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Medic extends Model
+{
+    use HasFactory;
+
+    public function specialty()
+    {
+        return $this->hasOne(Specialty::class, 'id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id');
+    }
+}
