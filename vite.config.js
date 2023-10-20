3fada923ce27ecc182784099b76efaf35aca3c53
Gustavo Matheus <guuhsykees@gmail.com>
2023-10-20 03:18:48 -0300

    build: first configs - init project

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
@@ -8,7 +9,19 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    ssr: {
+        noExternal: ["vue", "@protonemedia/laravel-splade"]
+    },
 });
